ae05bc7582411bdf44d427c1224d19b13766cd9d
Galal1129 <galal.ahmed@ogr.sakarya.edu.tr>
2021-01-08 16:26:54 +0300

    yasir2

---
+++ MyCreate/Migrations/20210106193211_News2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCreate.Migrations
+{
+    public partial class News2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "link",
+                table: "news",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "link",
+                table: "news");
+        }
+    }
+}
